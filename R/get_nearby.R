32b7d4a53af403be02659f860420bf021cabff58
Georg Andersson <skogskatt8@gmail.com>
2023-05-10 17:09:47 +0200

    Rewrote function as it did not find variables, still not work

--- R/get_nearby.R
+++ R/get_nearby.R
@@ -26,6 +26,7 @@ get_nearby <- function(df, radius=50, top=1, limited=TRUE, population_limit=0){
       filter(population > population_limit) %>%
       slice_min(distance, n = top) %>%  
       { if(limited) select(., name)  else select_all(.) }
+    
   }
   
   if(class(df) %>% first() %in% "sf") {
@@ -36,27 +37,18 @@ get_nearby <- function(df, radius=50, top=1, limited=TRUE, population_limit=0){
     
   }
   
-  locations <- df %>% 
+  locations <- df %>%
     mutate(ID = row_number()) %>%
-    group_by(ID) %>% 
-    nest() %>% 
-    ungroup() %>% 
-    mutate(loc = map(data, find_near)) %>% 
-    unnest(loc)
+    group_by(ID) %>%
+    nest() %>%
+    ungroup() %>%
+    mutate(loc = map(data, ~GNfindNearbyPlaceName(lat = lat , lng = lon, radius = radius, maxRows = "100", style = "MEDIUM") )) %>% 
+    unnest(loc) %>%
+    transmute(name = toponymName, distance = as.numeric(distance), population = as.numeric(population)) %>%
+    filter(population > population_limit) %>%
+    slice_min(distance, n = top) %>%  
+    { if(limited) select(., name)  else select_all(.) } %>% 
+  unnest(data)
   
   return(locations)
 }
-
-# tomte <- data.frame (sit_uid=c(11,12),lat=c(56,60),lon=c(16,16))
-# sites <- read_csv("sites_0915.csv")
-# names(sites) <- c("sit_uid","sit_nearby","lat","lon")
-# 
-# s2 <- filter(sites, sit_uid<=100)
-# 
-# s3 <- get_nearby(s2, limited = F, top = 10,population_limit = 1000)
-# 
-# s4<-merge(s3, s2,  by="sit_uid") 
-# 
-
-
-

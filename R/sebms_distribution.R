b98747ab67f769771c61af2a7c6cdadada04ce9e
Georg Andersson <skogskatt8@gmail.com>
2023-11-08 15:40:13 +0100

    Manage to get a decent map with terra and ggplot2()

--- R/sebms_distribution.R
+++ R/sebms_distribution.R
@@ -26,12 +26,10 @@ sebms_sites_map <- function(year=2021, width=5, height=3.5) {
     st_set_crs(3021)
   
   alla <- st_read("data-raw/figures/MapDistribution-data/R_files_for_similar_map/", "alla_2010-2014_sites") %>% 
-    st_set_crs(3021) %>% 
-    st_transform(3006)
+    st_set_crs(3021)
   
   bf <- st_read("data-raw/figures/MapDistribution-data/R_files_for_similar_map/", "ButterflySquares0423") %>% 
-    st_set_crs(3021) %>% 
-    st_transform(3006)
+    st_set_crs(3021)
   
   #a = rast("data-raw/figures/MapDistribution-data/R_files_for_similar_map/MapSweden.tif")
   
@@ -45,43 +43,26 @@ sebms_sites_map <- function(year=2021, width=5, height=3.5) {
     st_set_crs(3021) %>% 
     st_transform(3021)
   
-  ## FIXME: colours of map not good. Is there a map colour guide.
-  # TODO: Check out ggRGB in RStoolbox package
-  tiff <- terra::rast(system.file("extdata", "MapSweden.tif", 
-                           package = "sebmsR", mustWork = TRUE))
-  crs(tiff) <- "epsg:3006"
+  tiff1 <- terra::rast(system.file("extdata", "MapSweden_RGB.png", 
+                                  package = "sebmsR", mustWork = TRUE))
   
- tiff <-  raster::stack(system.file("extdata", "MapSweden.tif", 
-                            package = "sebmsR", mustWork = TRUE)) %>% 
-  raster::as.data.frame(xy=T)
- 
- 
- 
-  tifdf <- raster::brick(x = system.file("extdata", "MapSweden_RGB.tif",
-                                         package = "sebmsR", mustWork = TRUE), crs = "epsg:3006") %>%
-    as("SpatialPixelsDataFrame") %>% 
-    raster::as.data.frame(xy=T)
+  ext(tiff1) <- c(1179998, 1948697, 6129692, 7679610)
+  crs(tiff1) <- "epsg:3021"  
   
-  col_map <- function(rl) {
-    cm <- coltab(rl) %>% 
-      bind_rows() %>% 
-      mutate(hex = rgb(red, green, blue, maxColorValue = alpha),
-             values = paste0(value+1," = ", hex)) %>% 
-      pull()
-    #names(cm) <- 0:(length(cm) - 1)
-    cm
-  }
+  tiff <- tiff1 %>%   
+    terra::as.data.frame(xy=T) %>% 
+    rename_with(.fn = ~c("x", "y","Red", "Green", "Blue", "Max")) %>% 
+    filter(Red != 0) %>% 
+    select(-Max)
   
+  #terra::rasterize(as.matrix(tiff[1:2]), y= tiff1)
   
-  gplot(tiff, maxpixels = 1e6) +
-    geom_raster(aes(x = x, y = y, fill = value), show.legend = F) +
-    #geom_sf(data = bg) +
+  ggplot(data = tiff, aes(x = x, y = y)) +                   #plot map
+    geom_raster(aes(fill = rgb(r = Red, g = Green, b = Blue, maxColorValue = 255)), show.legend = FALSE) +
     geom_sf(data = bf, inherit.aes = F, alpha = 0, linewidth = 0.3, colour = "black") +
     geom_sf(data = alla, colour = "red", alpha = 0.2, inherit.aes = F) +
-    #scale_fill_distiller(type = "div", palette = "RdYlGn") +
-    # scale_fill_identity() +
-    scale_fill_gradientn(colours = colmap, guide = FALSE) +
-    theme_void()# + theme(panel.background = element_rect(fill = "white"))
+    scale_fill_identity() +
+    theme_void()
   
 }
 
@@ -131,35 +112,39 @@ sebms_distribution_map <- function(occ_sp, year=2021, species = 80, width= 5, he
   
   bg <- sebms_swe_borders %>%
     st_as_sf() %>% 
-    st_set_crs(3021) %>% 
+    st_set_crs(3857) %>% 
     st_transform(3021)
   
-  ## FIXME: colours of map not good. Is there a map colour guide.
-  tiff <- rast(system.file("extdata", "MapSweden.tif", 
-                           package = "sebmsR", mustWork = TRUE)) 
+  tiff1 <- terra::rast(system.file("extdata", "MapSweden_RGB.png", 
+                                  package = "sebmsR", mustWork = TRUE))
   
+  ext(tiff1) <- c(1179998, 1948697, 6129692, 7679610)
+  crs(tiff1) <- "epsg:3021"  
   
-  col_map <- function(rl) {
-    cm <- coltab(rl) %>% 
-      bind_rows() %>% 
-      mutate(hex = rgb(red, green, blue, maxColorValue = alpha),
-             values = paste0(value," = ", hex)) %>% 
-      pull()
-    #names(cm) <- 0:(length(cm) - 1)
-    cm
-  }
+  tiff <- tiff1 %>%   
+    terra::as.data.frame(xy=T) %>% 
+    rename_with(.fn = ~c("x", "y","Red", "Green", "Blue", "Max")) %>% 
+    filter(Red != 0)
   
-  terrain.colors(25)
-  
-  gplot(tiff, maxpixels = 1e6) +
-    geom_raster(aes(x = x, y = y, fill = factor(value)), show.legend = F) +
+  ggplot(data = tiff, aes(x = x, y = y)) +                   #plot map
+    geom_raster(aes(fill = rgb(r = Red, g = Green, b = Blue, maxColorValue = 255)), show.legend = FALSE) +
     #geom_sf(data = bg) +
     geom_sf(data = bf, inherit.aes = F, alpha = 0, linewidth = 0.3, colour = "black") +
     geom_sf(data = alla, colour = "red", alpha = 0.2, inherit.aes = F) +
-    #scale_fill_distiller(type = "div", palette = "RdYlGn") +
-    scale_fill_manual(values = col_map(tiff), guide = "none") +
-    theme_void()# + theme(panel.background = element_rect(fill = "white"))
+    scale_fill_identity() +
+    theme_void()
   
+  gplot(tiff, maxpixels = 1e6) +
+    geom_raster(aes(x = x, y = y, fill = factor(value)), show.legend = F) +
+    geom_sf(data = bg) +
+    geom_sf(data = bf, inherit.aes = F, alpha = 0, linewidth = 0.3, colour = "black") +
+    geom_sf(data = alla, colour = "red", alpha = 0.2, inherit.aes = F) +
+    scale_fill_manual(values = col_map(tiff), guide = "none") +
+    theme_void()
+  
+
+
+
 }
 
 

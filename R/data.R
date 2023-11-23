99cc3b46c5c5db78f74ce9057b8b62201dd52f89
Georg Andersson <skogskatt8@gmail.com>
2023-11-23 16:49:00 +0100

    Add data function for species occurences with coordinates

--- R/data.R
+++ R/data.R
@@ -482,3 +482,102 @@ sebms_naturum_climate <- function() {
 #  globalVariables(names = unlist(strsplit(split = " ",
 # paste0("word1 ",
 #  "word2"))))
+
+
+
+#' Retrieve Species Abundance List per Site with Coordinates
+#'
+#' Returns a data frame of counts per species, site, with coordinates in SWEREF.
+#' This is used for the [sebms_distribution_map()]
+#'
+#' @inheritParams sebms_abundance_per_species_plot
+#'  
+#' @import tibble
+#' @importFrom glue glue
+#' @import dplyr
+#' @importFrom stringr str_detect str_to_lower
+#' @importFrom DBI dbGetQuery
+#'
+#' @returns a tibble with species ids and names, filtered for trimed species
+#'   names, the site ids and names, site type, number of individuals for eahc
+#'   species, the date, county, region, and municipality
+#' @export
+sebms_occurances_distribution <- function(year = 2020:2021, Art = 1:200, Län = ".", Landskap = ".", Kommun = ".", source = c(54,55,56,63,64,66,67,84)) {
+  
+  year <- glue("({paste0({year}, collapse = ',')})") # Make year span to a vector of years for the SQL
+  Art <- glue("({paste0({Art}, collapse = ',')})")
+  source <- glue("({paste0({source}, collapse = ',')})")
+  
+  Län <- paste0(str_to_lower(Län),collapse = "|") # Make the list of Län to s regex statement
+  Landskap <- paste0(str_to_lower(Landskap),collapse = "|") # Make the list of Landskap to s regex statement
+  Kommun <- paste0(str_to_lower(Kommun),collapse = "|") # Make the list of Kommun to s regex statement
+  
+  county <- regID %>% 
+    filter(str_detect(reg_name, Län)) %>% # Filter out matching Län from a look up table
+    pull(reg_uid) %>% # pull out the id-numbers
+    paste0(collapse = ',') # Make the id-numbers a vector palatable to the SQL
+  
+  region <- regID %>% 
+    filter(str_detect(reg_name, Landskap)) %>% 
+    pull(reg_uid) %>% 
+    paste0(collapse = ',') # Make the id-numbers a vector palatable to the SQL
+  
+  municipality <- regID %>% 
+    filter(str_detect(reg_name, Kommun)) %>% 
+    pull(reg_uid) %>% 
+    paste0(collapse = ',') # Make the id-numbers a vector palatable to the SQL
+  
+  
+  q <- glue("
+          WITH reg AS
+           (SELECT reg_uid AS reg_id, reg_name AS län
+             FROM reg_region
+             WHERE reg_uid IN ({county}) AND reg_group = 'C'),
+           lsk AS
+           (SELECT reg_uid AS landskaps_id, reg_name AS landskap
+             FROM reg_region
+             WHERE reg_uid IN ({region}) AND reg_group = 'P'),
+           mun AS
+           (SELECT reg_uid AS kommun_id, reg_name AS kommun
+             FROM reg_region
+             WHERE reg_uid IN ({municipality}) AND reg_group = 'M')
+   
+        SELECT
+          spe.spe_uid AS speuid,
+          spe.spe_semainname As art,
+          --sit.sit_type AS sitetype,
+          SUM(obs.obs_count) AS antal,
+          --extract('YEAR' from vis_begintime) AS years,
+          --vis_begintime::date as Datum,
+          sit.sit_name AS Lokalnamn,
+          sit.sit_geosweref99tmlat AS lat,
+          sit.sit_geosweref99tmlon AS lon,
+          reg.län,
+          lsk.landskap,
+          mun.kommun
+        FROM obs_observation AS obs
+        
+        INNER JOIN vis_visit AS vis ON obs.obs_vis_visitid = vis.vis_uid
+        INNER JOIN spe_species AS spe ON obs.obs_spe_speciesid = spe.spe_uid
+        INNER JOIN seg_segment AS seg ON obs.obs_seg_segmentid = seg.seg_uid
+        INNER JOIN sit_site AS sit ON seg.seg_sit_siteid = sit.sit_uid
+        INNER JOIN  spv_speciesvalidation AS spv ON spe.spe_uid = spv_spe_speciesid 
+        INNER JOIN reg ON sit.sit_reg_countyid = reg.reg_id
+        INNER JOIN lsk ON sit.sit_reg_provinceid = lsk.landskaps_id
+        INNER JOIN mun ON sit.sit_reg_municipalityid = mun.kommun_id
+        WHERE
+          extract('YEAR' from vis_begintime) IN {year}
+          AND vis_typ_datasourceid IN {source}
+          AND ( spv.spv_istrim=TRUE OR spe_uid IN (135,131,132,133,139) ) -- Include nullobs and 4 aggregated species groups
+          AND spe.spe_uid IN {Art}
+        
+        GROUP BY
+          spe.spe_uid, Lokalnamn, lat, lon, reg.reg_id, reg.län, lsk.landskaps_id, lsk.landskap, mun.kommun_id, mun.kommun --, years, Datum, 
+        ORDER BY
+          antal DESC;")
+  
+  sebms_pool <- sebms_assert_connection()
+  res <- dbGetQuery(sebms_pool, q)
+  as_tibble(res)
+  
+} 
